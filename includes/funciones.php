db8b42ab23bde0c5bad32b472e5ff57b7a5e3cb8
janethcj <78242681+janethcj@users.noreply.github.com>
2021-04-22 01:26:45 -0500

    Update funciones.php

--- includes/funciones.php
+++ includes/funciones.php
@@ -5,7 +5,7 @@
 
 <?php
 
-function obtenerServicios(){
+function obtenerServicios() : array{
   try{
     //Importar conexión a BD
     require 'database.php';
@@ -16,17 +16,22 @@ function obtenerServicios(){
 
     //Arreglo vacio
     $servicios = [];
+    $i = 0;
 
     //Obtener resultados (TODOS)
     while($row = pg_fetch_assoc($consulta)){
-      echo "<pre>";
-      var_dump($row);
-      echo "</pre>";
+        $servicios[$i]['id'] = $row['id'];
+        $servicios[$i]['nombre'] = $row['nombre'];
+        $servicios[$i]['precio'] = $row['precio'];
+
+
+        $i++;
     }
 
-    echo "<pre>";
+    return
+    /*echo "<pre>";
     var_dump(json_encode($servicios));
-    echo "</pre>";
+    echo "</pre>";*/
 
   }catch(\Throwable $th){
     var_dump($th);
